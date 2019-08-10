709e85ce9e9f83708de7809bc8ea230310854975
Kremer Anna <sofiakremer7925@gmail.com>
2019-08-10 17:07:37 +0700

    fix main.c

--- src/main.c
+++ src/main.c
@@ -32,6 +32,32 @@ sortWords(words);
 
     break;
 }
+
+case 3:
+  {
+    length = readFromFileToArray(&text, argv[1]);
+    if(length == 0) {
+      printf("Невозможно открыть файл для чтения!\n");
+      return -1;
+    }
+
+    char **words = parseWords(text);
+    sortWords(words);
+
+    FILE *out = fopen(argv[2], "w");
+    if(!out) {
+      printf("Невозможно открыть файл для записи!\n");
+      return -1;
+    }
+
+    while(*words != NULL) {
+      fprintf(out, "%s\n", *words);
+      words++;
+    }
+
+    break;
+  }
+
   default:
     printf("Пожалуйста укажите путь до текста!\n");
     return -1;
